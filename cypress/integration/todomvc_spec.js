33a6add08f25caa4a6d39ad8484f83c012327783
Alexandr Marinko <alexandr.marinko@gmail.com>
2018-08-10 10:52:25 +0200

    Initial skeleton

---
+++ cypress/integration/todomvc_spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+context('TodoMVC', () => {
+  before(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  context('', () => {
+    it('', () => {
+      
+    });
+  })
+})
---
+++ cypress/page-objects/app.page.js
@@ -0,0 +1,15 @@
+const selectors = {
+  header: 'header',
+  title: 'title',
+  newTodoInput: 'new-todo-input',
+  mainSection: 'main-section',
+  todoList: 'todo-list',
+  todoItem: 'todo-item',
+  todoItemLabel: 'todo-item-label',
+  todoItemToggleBtn: 'toggle',
+  todoItemRemove: 'todo-item-remove',
+};
+
+export default {
+  selectors,
+};
